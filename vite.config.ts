b36f7629cdd27244a51dbbc1761768b4c651d713
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-02-28 09:31:44 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: '/ticket-generator/',
   resolve: {
     alias: {
       '@': '/src',
